d17ad9a01bb9ff3e5009e765ffbf9d196fe75da6
rchandanaharshitha-netizen <rchandanaharshitha@gmail.com>
2026-05-26 22:30:27 +0530

    Update machine_learning_curated.py

--- stedi-project/scripts/machine_learning_curated.py
+++ stedi-project/scripts/machine_learning_curated.py
@@ -12,7 +12,6 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Read step trainer trusted from S3
 step_trainer_df = glueContext.create_dynamic_frame.from_options(
     format_options={"multiline": False},
     connection_type="s3",
@@ -24,7 +23,6 @@ step_trainer_df = glueContext.create_dynamic_frame.from_options(
     transformation_ctx="step_trainer_trusted"
 ).toDF()
 
-# Read accelerometer trusted from S3
 accelerometer_df = glueContext.create_dynamic_frame.from_options(
     format_options={"multiline": False},
     connection_type="s3",
@@ -36,30 +34,29 @@ accelerometer_df = glueContext.create_dynamic_frame.from_options(
     transformation_ctx="accelerometer_trusted"
 ).toDF()
 
-print("Step trainer trusted count :", step_trainer_df.count())
-print("Accelerometer trusted count:", accelerometer_df.count())
-
-# Join on timestamp = sensorreadingtime
 joined_df = step_trainer_df.join(
     accelerometer_df,
     step_trainer_df["sensorreadingtime"] == accelerometer_df["timestamp"],
     "inner"
 )
 
-print("Machine learning curated count:", joined_df.count())
+ml_curated = DynamicFrame.fromDF(
+    joined_df, glueContext, "machine_learning_curated"
+)
 
-# Write to machine learning curated S3 location
-glueContext.write_dynamic_frame.from_options(
-    frame=DynamicFrame.fromDF(
-        joined_df, glueContext, "machine_learning_curated"
-    ),
+sink = glueContext.getSink(
+    path="s3://stedi-project-chandana/machine_learning/curated/",
     connection_type="s3",
-    format="json",
-    connection_options={
-        "path": "s3://stedi-project-chandana/machine_learning/curated/",
-        "partitionKeys": []
-    },
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=[],
+    enableUpdateCatalog=True,
     transformation_ctx="write_ml_curated"
 )
+sink.setCatalogInfo(
+    catalogDatabase="stedi",
+    catalogTableName="machine_learning_curated"
+)
+sink.setFormat("json")
+sink.writeFrame(ml_curated)
 
 job.commit()
