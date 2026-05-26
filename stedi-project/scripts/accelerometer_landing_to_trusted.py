c4d5339f0a56e177de8fee9f8ae158c685850438
rchandanaharshitha-netizen <rchandanaharshitha@gmail.com>
2026-05-26 22:28:25 +0530

    Update accelerometer_landing_to_trusted.py

--- stedi-project/scripts/accelerometer_landing_to_trusted.py
+++ stedi-project/scripts/accelerometer_landing_to_trusted.py
@@ -12,39 +12,57 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+accelerometer_df = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://stedi-project-chandana/accelerometer/landing/"],
+        "recurse": True
+    },
+    transformation_ctx="accelerometer_landing"
+).toDF()
+
 customer_df = glueContext.create_dynamic_frame.from_options(
     format_options={"multiline": False},
     connection_type="s3",
     format="json",
     connection_options={
-        "paths": ["s3://stedi-project-chandana/customer/landing/"],
+        "paths": ["s3://stedi-project-chandana/customer/trusted/"],
         "recurse": True
     },
-    transformation_ctx="customer_landing"
+    transformation_ctx="customer_trusted"
 ).toDF()
 
-customer_trusted_df = customer_df.filter(
-    (customer_df["sharewithresearchasofdate"].isNotNull()) &
-    (customer_df["sharewithresearchasofdate"] > 0)
+joined_df = accelerometer_df.join(
+    customer_df,
+    accelerometer_df["user"] == customer_df["email"],
+    "inner"
+).select(
+    accelerometer_df["timestamp"],
+    accelerometer_df["user"],
+    accelerometer_df["x"],
+    accelerometer_df["y"],
+    accelerometer_df["z"]
 )
 
-customer_trusted = DynamicFrame.fromDF(
-    customer_trusted_df, glueContext, "customer_trusted"
+accelerometer_trusted = DynamicFrame.fromDF(
+    joined_df, glueContext, "accelerometer_trusted"
 )
 
 sink = glueContext.getSink(
-    path="s3://stedi-project-chandana/customer/trusted/",
+    path="s3://stedi-project-chandana/accelerometer/trusted/",
     connection_type="s3",
     updateBehavior="UPDATE_IN_DATABASE",
     partitionKeys=[],
     enableUpdateCatalog=True,
-    transformation_ctx="write_customer_trusted"
+    transformation_ctx="write_accelerometer_trusted"
 )
 sink.setCatalogInfo(
     catalogDatabase="stedi",
-    catalogTableName="customer_trusted"
+    catalogTableName="accelerometer_trusted"
 )
 sink.setFormat("json")
-sink.writeFrame(customer_trusted)
+sink.writeFrame(accelerometer_trusted)
 
 job.commit()
